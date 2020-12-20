2bec0e3d73820bfeb38fb800db5b59575713fa2b
Carmen Morado <cmora171@fiu.edu>
2020-12-19 19:56:59 -0500

    Pictures are now able to be imported using UIImagePickerController and within the imagePickerController method. A custom subclass of NSObject has been created to hold data for the app (the name and photo for every person, which will additionally be stored in a Person array within ViewController). In addition, the collection view now displays the correct number of items, the correct information inside each cell, and when users tap a picture they can set a person's name thanks to the collectionView method.

---
+++ Project10/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Carmen Morado on 12/19/20.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}

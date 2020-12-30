ab64ee7840e9d80533c11441867b40b788f55c3d
Carmen Morado <cmora171@fiu.edu>
2020-12-30 16:15:55 -0500

    The Person class has been modified so that it conforms to Codable. A save() method has been added to the view controller that uses the JSONEncoder class to convert the people array into a Data object, which can then be saved to UserDefaults. This conversion might fail, so if let and try? are used so that data is saved only when the JSON conversion was successful. Self?.save() is called just before calling self.collectionView.reloadData() and save() is also called just before the end of the imagePickerController method. Moreover, the new code added in viewDidLoad is needed to load the array back from disk when the app runs.The object(forKey:) method is used to pull out an optional Data, using if let and as? to unwrap it. Then, that is given to an instance of JSONDecoder to convert it back to an object graph.

--- Project10/ViewController.swift
+++ Project10/ViewController.swift
@@ -15,6 +15,14 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
         
+        let defaults = UserDefaults.standard
+
+        if let savedPeople = defaults.object(forKey: "people") as? Data {
+            if let decodedPeople = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(savedPeople) as? [Person] {
+                people = decodedPeople
+            }
+        }
+        
         self.navigationItem.title = "Portraits"
     }
     
@@ -63,6 +71,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         
         let person = Person(name: "Unknown", image: imageName)
         people.append(person)
+        save()
         collectionView.reloadData()
 
         dismiss(animated: true)
@@ -84,6 +93,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] _ in
             guard let newName = ac?.textFields?[0].text else { return }
             person.name = newName
+            self?.save()
             self?.collectionView.reloadData()
         })
         
@@ -93,6 +103,13 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         })
 
         present(ac, animated: true)
+    }
+    
+    func save() {
+        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: people, requiringSecureCoding: false) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "people")
+        }
     }
 
 
